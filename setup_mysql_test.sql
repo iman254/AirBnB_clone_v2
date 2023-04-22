e76383e5bd2b6ee0e3e7303251fd3ac85bb84763
iman254 <stephaniemarquinz@icloud.com>
2023-04-22 04:58:15 +0300

    mysql setup

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
---it creates database and a user
+-- it creates database and a user
 
-CREATE DATABASE IF NOT EXIST hbtn_test_db;
-CREATE USER IF NOT EXIST 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER If NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
