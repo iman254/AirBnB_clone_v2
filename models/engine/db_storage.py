32dcc6292465f3f4217c40202feab9930c0f81b0
iman254 <stephaniemarquinz@icloud.com>
2023-04-22 06:37:13 +0300

    tasks

---
+++ models/engine/db_storage.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+""" database storage management """
+from sqlalchemy import create_engine
+import os
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+
+user = os.environ.get('HBNB_MYSQL_USER')
+password = os.environ.get('HBNB_MYSQL_PWD')
+host = os.environ.get('HBNB_MYSQL_HOST', 'localhost')
+database = os.environ.get('HBNB_MYSQL_DB')
+
+
+class DBStorage():
+    """ a class that efines mtributes and methods for the database """
+
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """ class initializeR"""
+        self.__engine = create_engine(
+            "mysql+mysqldb://{}:{}@{}/{}".format(user,
+                                                 password,
+                                                 host,
+                                                 database),
+            pool_pre_ping=True
+        )
+        hbnd_env = os.environ.get('HBNB_ENV')
+        if (hbnd_env == "test"):
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """ retrives all objects """
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+        Session = sessionmaker(bind=self.__engine)
+        self.__session = Session()
+        objects = {}
+        if cls is not None:
+            # Query for objects of a specific class
+            results = self.__session.query(cls).all()
+        else:
+            # Query for all types of objects
+            results = []
+            for cls in [State, City, User, Place, Review, Amenity]:
+                results.extend(self.__session.query(cls).all())
+        # Add objects to dictionary
+        for obj in results:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            objects[key] = obj
+        return objects
+
+    def new(self, obj):
+        """ add a new object to the session """
+
+        self.__session.add(obj)
+
+    def save(self):
+        """ saves an object to the database """
+
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ deletes an object from current session """
+        if (obj):
+            self.__session.delete(obj)
+
+    def reload(self):
+        """reloads objects from the database"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        # create all tables in the database
+        Base.metadata.create_all(self.__engine)
+
+        # create the current database session
+        Session = scoped_session(sessionmaker(bind=self.__engine,
+                                              expire_on_commit=True))
+        self.__session = Session()
+
+    def close(self):
+        """closes the current database session"""
+        self.__session.close()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
-
-class State(BaseModel):
-    """ State class """
-    name = ""
+class City(BaseModel, Base):
+    """ The class city, comprises of state ID and name """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship('Place', backref='cities', cascade="all, delete-orphan")
