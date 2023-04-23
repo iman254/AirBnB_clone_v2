a0fa75faf3734e9744a9c074ff7977265df7ffba
iman254 <stephaniemarquinz@icloud.com>
2023-04-24 01:04:32 +0300

    task 3

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-"""a scriopt that starts flask w.app"""
-
+"""script thaT display “Hello HBNB!”"""
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def index():
-    """function that return hello HBNB"""
+    """a function that returns hello HBNB"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def index_hbnb():
-    """function that return HBNB"""
+    """a function to return HBNB"""
     return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def index_c(text):
+    """a function that returns C is fun"""
+    return 'C ' + text.replace('_', ' ')
 
 
 if __name__ == "__main__":
