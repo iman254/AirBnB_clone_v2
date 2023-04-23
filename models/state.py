42a4e99beb185921998eebb44853b5e06cc6ef0c
iman254 <stephaniemarquinz@icloud.com>
2023-04-24 02:44:45 +0300

    task

--- models/state.py
+++ models/state.py
@@ -4,10 +4,25 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
+from models import storage
+from models.city import City
+
 
 class City(BaseModel, Base):
-    """ The class city, comprises of state ID and name """
-    __tablename__ = "cities"
+    """ State a class """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    places = relationship('Place', backref='cities', cascade="all, delete-orphan")
+
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Returns a list of the city instance containing state_id
+            equals to the current state_id"""
+            values_city = storage.all(City).values()
+            list_city = []
+            for city in values_city:
+                if city.state_id == self.id:
+                    list_city.append(city)
+            return list_city
