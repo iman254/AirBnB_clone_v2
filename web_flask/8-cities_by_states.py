7123cde1d5070c2c405bcf53aefb6bd28268eafd
iman254 <stephaniemarquinz@icloud.com>
2023-04-24 03:44:11 +0300

    task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -21,5 +21,6 @@ def states_list():
     states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
